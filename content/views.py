3e9fef711c9fdbc98ac066479e41643b0ebdcd82
minjun <rapthon@naver.com>
2022-02-05 19:29:09 +0900

    frontend is completed , constructing DB

--- content/views.py
+++ content/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework.views import APIView
+from .models import Feed
 
-# Create your views here.
+class Main(APIView):
+    def get(self, request):
+        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed order by '-id';
+
+        return render(request, "instagram\main.html", context=dict(feed_list=feed_list))
