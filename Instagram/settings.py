191ec2a25ddee2fc89ff19d4bce590d253f66a0b
minjun <rapthon@naver.com>
2022-02-09 18:47:35 +0900

    implements feed uploading
    browser -> ajax -> views.py -> DB

--- Instagram/settings.py
+++ Instagram/settings.py
@@ -8,10 +8,10 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -23,7 +23,6 @@ SECRET_KEY = 'django-insecure-&f*_s@xv+0c=jc04tb^n)*q3%j!(v&kp%8x!evgnc08=77568y
 DEBUG = True
 
 ALLOWED_HOSTS = []
-
 
 # Application definition
 
@@ -55,8 +54,7 @@ ROOT_URLCONF = 'Instagram.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,7 +69,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'Instagram.wsgi.application'
 
-
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
@@ -81,7 +78,6 @@ DATABASES = {
         'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
@@ -101,7 +97,6 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
@@ -115,7 +110,6 @@ USE_L10N = True
 
 USE_TZ = True
 
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
@@ -125,3 +119,7 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# 각 media 파일에 대한 URL Prefix
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
--- content/views.py
+++ content/views.py
@@ -1,9 +1,36 @@
 from django.shortcuts import render
+from rest_framework.response import Response
 from rest_framework.views import APIView
 from .models import Feed
+from uuid import uuid4
+import os
+from Instagram.settings import MEDIA_ROOT
+
 
 class Main(APIView):
+    # noinspection PyMethodMayBeStatic
     def get(self, request):
-        feed_list = Feed.objects.all().order_by('-id') # select * from content_feed order by '-id';
+        feed_list = Feed.objects.all().order_by('-id')
+        return render(request, "instagram/main.html", context=dict(feed_list=feed_list))
 
-        return render(request, "instagram\main.html", context=dict(feed_list=feed_list))
+
+class UploadFeed(APIView):
+    # noinspection PyMethodMayBeStatic
+    def post(self, request):
+        file = request.FILES['file']
+
+        uuid_name = uuid4().hex
+        save_path = os.path.join(MEDIA_ROOT, uuid_name)
+
+        with open(save_path, 'wb+') as destination:
+            for chunk in file.chunks():
+                destination.write(chunk)
+
+        image = uuid_name
+        content = request.data.get('content')
+        profile_image = request.data.get('profile_image')
+        user_id = request.data.get('user_id')
+
+        Feed.objects.create(content=content, image=image, profile_image=profile_image, user_id=user_id, like_count=0)
+
+        return Response(status=200)
